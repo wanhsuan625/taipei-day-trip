586d83b0d134f371f91e57b50db29774aca083ab
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2022-12-03 01:43:30 +0800

    add slide of images

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,65 +1,104 @@
-let isLoading = false;
+// WEBSITE ADDRESS
+// let url = location.pathname.split("/")[2];
+let eachAttractionFetch = (num) => {
+    fetch(`http://54.199.123.84:3000/api/attraction/${num}`)
+    .then(response => {
+        return response.json();})
+    .then(data => {
+        console.log(data);
+        attractionImg(data);
+        attractionInformation(data);
+    })
+}
+eachAttractionFetch(10);
+
 
 // CREATE DOM
-const imgSize = document.querySelector(".imgSize");
+const imgBox = document.querySelector(".imgBox");
+const arrow = document.querySelector(".arrow");
 const infoBox = document.querySelector(".infoBox");
 const bookingForm = document.querySelector(".bookingForm");
 const main = document.querySelector("main");
 const address = document.querySelector(".address");
 const transport = document.querySelector(".transport");
 
-let eachAttraction = (data) =>{
+let attractionImg = (data) =>{
     let result = data.data;
     let len = result.images.length;
-    // 待修正
+    
     // IMAGES
-    let img = document.createElement("img");
-    img.className = "attractionImg";
-    img.src = result.images[0];
-    imgSize.appendChild(img);
+    for(let i = 0; i < len; i++){
+        let imgSlide = document.createElement("div");
+        imgSlide.className = "imgSlide";
+        imgBox.insertBefore(imgSlide, arrow);
+
+        let img = document.createElement("img");
+        img.className = "attractionImg";
+        img.src = result.images[i];
+        imgSlide.appendChild(img);
+    }
+
+     // BOTTOM DOT
+     let dotGroup = document.createElement("div");
+     dotGroup.className = "dotGroup";
+     imgBox.appendChild(dotGroup);
+     for(let i = 0; i < len; i++){
+         let dot = dotGroup.appendChild(document.createElement("div"));
+         dot.className = "dot";
+         dotGroup.appendChild(dot);
+     }
+
+    // SLIDE IMAGES
+    let slide = document.querySelectorAll(".imgSlide");
+    let dot = document.querySelectorAll(".dot");
+    
+
+
+    
+}
+
+
+
+// SLIDE IMAGE
+// let slideIndex = 1;
+// slideShow(slideIndex);
+
+// let currentSlide = (n) => { slideShow(slideIndex = n);}
+// let clickSlide = (n) => { slideShow(slideIndex += n);}
+
+// function slideShow(n){
+//     if (n > slide.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slide.length}
+//     for (i = 0; i < slide.length; i++) {
+//         slide[i].style.display = "none";
+//         dot[i].className = dot[i].className.replace(" active", "");
+//     }
+//     slide[slideIndex-1].style.display = "block";
+//     dot[slideIndex-1].className += " active";
+// }
+
+let attractionInformation = (data) =>{
+    let result = data.data;
 
     // ATTRACTION - NAME , MRT AND CATEGORY
     let name = document.createElement("h1");
-    name.textContent = result.name;
+    name.innerHTML = result.name;
     infoBox.insertBefore(name, bookingForm);
     
     let site = document.createElement("p");
-    site.textContent = result.category + " at " + result.mrt;
+    site.innerHTML = result.category + " at " + result.mrt;
     infoBox.insertBefore(site, bookingForm);
 
     // INFORMATION
     let content1 = document.createElement("div");
-    content1.textContent = result.description;
+    content1.innerHTML = result.description;
     main.insertBefore(content1, address);
 
     let content2 = document.createElement("div");
-    content2.textContent = result.address;
+    content2.innerHTML = result.address;
     main.insertBefore(content2, transport);
 
     let content3 = document.createElement("div");
-    content3.textContent = result.transport;
+    content3.innerHTML = result.transport;
     main.appendChild(content3);
 }
-
-async function eachAttractionFetch(num){
-    isLoading = true;
-
-    let eachAPI = await fetch(`http://54.199.123.84:3000/api/attraction/${num}`);
-    let eachData = await eachAPI.json();
-    console.log(eachData); 
-    eachAttraction(eachData);   
-
-    isLoading = false;
-}
-eachAttractionFetch(4);
-
-// CHANGE PAYMENT
-// const afternoon = document.querySelector("#afternoon");
-// const morning = document.querySelector("#morning");
-// const changeMoney = document.querySelector(".changeMoney")
-// afternoon.addEventListener("click",()=>{
-//     changeMoney.textContent = "新台幣 2500 元"
-// })
-// morning.addEventListener("click",()=>{
-//     changeMoney.textContent = "新台幣 2000 元"
-// })
