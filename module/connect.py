4973fe3fb833d797fca1d35f7fc7f7d608946710
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2022-12-08 12:24:27 +0800

    add env in module

--- module/connect.py
+++ module/connect.py
@@ -1,14 +1,17 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def db_connection():
     dbconfig = {
-        "host" : "localhost",
-        "user" : "root",
-        "password" : "aa22bb33",
-        "database" : "taipeidaytrip",
-        "pool_name" : "my_pool",
-        "pool_size" : 5,
-        "pool_reset_session" : True,
+        "host" : os.getenv("host"),
+        "user" : os.getenv("user"),
+        "password" : os.getenv("password"),
+        "database" : os.getenv("database"),
+        "pool_name" : os.getenv("pool_name"),
+        "pool_size" : int(os.getenv("pool_size")),
+        "pool_reset_session" : os.getenv("pool_reset_session"),
     }
     connection_pool = mysql.connector.pooling.MySQLConnectionPool(
         **dbconfig
