2ddc537f1d8f90916426edfa199bb487adfc16b3
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2022-12-17 10:31:00 +0800

    revise setting of total price

--- static/js/booking.js
+++ static/js/booking.js
@@ -6,6 +6,7 @@ let bookHeadline = document.createElement("div");
 bookHeadline.className = "book_headline";
 let inputName = document.getElementById("name");
 let inputEmail = document.getElementById("email");
+const totalPrice = document.querySelector(".total_price");
 const footer = document.querySelector("footer");
 
 // USER INFORMATION
@@ -25,14 +26,14 @@ async function fetchBooking(){
     let bookingData = await fetch("/api/booking").then(response => {return response.json()});
 
     if(bookingData.data == null){
-        DOMofNoBooking();
+        DOMof_NoBooking();
     }
     else{
         DOMofBooking(bookingData);
     }
 }
 
-let DOMofNoBooking = () => {
+let DOMof_NoBooking = () => {
     main.innerHTML = "";
     let article = document.createElement("article");
     article.className = "book_container nobooking";
@@ -45,41 +46,43 @@ let DOMofNoBooking = () => {
 
 let DOMofBooking = (data) => {
     let result = data.data;
-        let result_attraction = data.data.attraction;
+    let result_attraction = data.data.attraction;
 
-        let time;
-        if(result["time"] == "morning"){
-            time = "早上8點至下午1點";
-        }
-        else{
-            time = "下午1點至晚上6點";
-        }
+    let time;
+    if(result["time"] == "morning"){
+        time = "早上8點至下午1點";
+    }
+    else{
+        time = "下午1點至晚上6點";
+    }
 
-        let bookingContent = `<article class="book_container">
-                                <img class="book_img" src=${result_attraction["image"]} ,alt="">
+    let bookingContent = `<article class="book_container">
+                            <img class="book_img" src=${result_attraction["image"]} ,alt="">
 
-                                <section class="book_info_container">
-                                    <h1>台北一日遊：${result_attraction["name"]}</h1>
-                                    <div class="book_detail"><span>日期：</span>${result["date"]}</div>
-                                    <div class="book_detail"><span>時間：</span>${time}</div>
-                                    <div class="book_detail"><span>費用：</span>新台幣 ${result["price"]}元</div>
-                                    <div class="book_detail"><span>地點：</span>${result_attraction["address"]}</div>
-                                </section>
+                            <section class="book_info_container">
+                                <h1>台北一日遊：${result_attraction["name"]}</h1>
+                                <div class="book_detail"><span>日期：</span>${result["date"]}</div>
+                                <div class="book_detail"><span>時間：</span>${time}</div>
+                                <div class="book_detail"><span>費用：</span>新台幣 ${result["price"]}元</div>
+                                <div class="book_detail"><span>地點：</span>${result_attraction["address"]}</div>
+                            </section>
 
-                                <img class="delete_icon" src="/image/icon_delete.png" alt="">
-                            </article>`
+                            <img class="delete_icon" src="/image/icon_delete.png" alt="">
+                        </article>`
 
-        main.insertAdjacentHTML("afterbegin", bookingContent);
+    main.insertAdjacentHTML("afterbegin", bookingContent);
 
-        const deleteIcon = document.querySelector(".delete_icon");
-        deleteIcon.addEventListener("click", () => {
-            fetchDeletItinerary();
-            location.reload();
-        });
+    // DELETE ITINERARY
+    const deleteIcon = document.querySelector(".delete_icon");
+    deleteIcon.addEventListener("click", () => {
+        fetchDeletItinerary();
+        location.reload();
+    });
+
+    // TOTAL PRICE
+    totalPrice.textContent = `總價：新台幣 ${result["price"]} 元`;
 }
 
-
-// --- DELETE ITINERARY -------------------------------------------------------------------
 let fetchDeletItinerary = () => {
     fetch("/api/booking",{
         method: "DELETE",
@@ -88,13 +91,11 @@ let fetchDeletItinerary = () => {
         return response.json()
     }).then(data => {
         if (data.ok == "true") {
-            DOMofNoBooking();
+            DOMof_NoBooking();
         }
         return
     })
 }
-
-
 
 
 // --- CREDIT CARD ------------------------------------------------------------------------
