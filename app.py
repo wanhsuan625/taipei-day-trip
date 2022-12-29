f5f65e10884b06acc1e260fc9e0d3e1db65d356c
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2022-12-29 17:43:55 +0800

    feat: add member page

--- app.py
+++ app.py
@@ -39,7 +39,10 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
+@app.route("/member")
+def member():
+	return render_template("member.html")
 
 if __name__ == "__main__":
-	# app.run(port=3000,debug=True)
-	app.run(host="0.0.0.0",port=3000,debug=True)
+	app.run(port=3000,debug=True)
+	# app.run(host="0.0.0.0",port=3000,debug=True)
