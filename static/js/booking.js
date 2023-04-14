8f2b623885ed76a3b7a6e226049b3d451626c94b
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-04-14 21:38:02 +0800

    feat : add the function of linking to other attraction pages

--- static/js/booking.js
+++ static/js/booking.js
@@ -70,9 +70,70 @@ let DOMof_No_Booking = () => {
     article.className = "booking-container no-booking";
     article.textContent = "目前沒有任何待預定的行程";
     main.appendChild(article);
+    
+    //  推薦景點
+    let recommend_attraction_title = 
+    '<article class="recommend__container">\
+        <h4 class="recommend__title">您可能會有興趣的其他景點：</h4>\
+        <div class="recommend__attraction-combination"></div>\
+    </article>\
+    ';
+    main.insertAdjacentHTML("afterend", recommend_attraction_title);
+    const attraction_combination = document.querySelector(".recommend__attraction-combination");
+
+    let recommend_number = [];
+    for ( let i = 0; i < 4 ; i++ ){
+    let random = Math.floor( Math.random() * 57 ) + 1 ;
+
+    if ( recommend_number.includes( random ) ){
+        recommend_number.push( random + 1 );
+        random++ ;
+    }else{
+        recommend_number.push( random );}
+
+    // RECOMMEND ATTRACTION DOM
+    const recommend_img_box = document.createElement("div");
+    recommend_img_box.className = "recommend__attraction-imgBox";
+    attraction_combination.appendChild(recommend_img_box);
+
+    // 景點圖片
+    const recommend_img = document.createElement("img");
+    recommend_img.className = "recommend__attraction-img";
+    recommend_img_box.appendChild(recommend_img);
+
+    // 景點名稱
+    const recommend_attraction_name = document.createElement("div");
+    recommend_attraction_name.className = "recommend__attraction-name";
+    recommend_img_box.appendChild(recommend_attraction_name);
+
+    const recommend_attraction_descript = document.createElement("div");
+    recommend_attraction_descript.className = "recommend__attraction-descript";
+
+    fetch(`/api/attraction/${random}`)
+    .then( response => { return response.json(); })
+    .then( data => {
+        let result = data.data;
+        let attraction_img = result.images[0];
+        let attraction_name = result.name;
+        let attraction_description = result.description.split("。")[0];
+
+        recommend_img.src = attraction_img;
+        recommend_attraction_name.innerText = attraction_name;
+        
+        recommend_attraction_name.appendChild(recommend_attraction_descript);
+        recommend_attraction_descript.textContent = attraction_description;
+        // 景點詳述
+        // recommend_attraction_name.insertAdjacentText("beforeend", attraction_description);
+    })
+
+    // 點選 - 跳轉至該景點頁面
+    recommend_img_box.addEventListener("click", () => {
+        window.location.href = `/attraction/${random}`;
+    })
+    }
 
     footer.style.paddingTop = "50px";
-    footer.style.paddingBottom = "100%";
+    footer.style.paddingBottom = "20%";
 }
 
 let DOMofBooking = (data) => {
